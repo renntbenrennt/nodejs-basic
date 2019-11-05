cce80d4e2d4ba7bd9ca6387c2092f1c6cf680284
Benjamin <benjamin.voila@gmail.com>
2019-11-05 17:12:46 +0800

    note: finish taking the snippet I want in this section

--- nodejs-in-action/2.7 Using Callback/blog_recent.js
+++ nodejs-in-action/2.7 Using Callback/blog_recent.js
@@ -10,7 +10,7 @@ const fs = require('fs');
  */
 // http.createServer((req, res) => {
 //     if (req.url === '/') {
-//         fs.readFile('./title.json', (err, data) => {
+//         fs.readFile('./titles.json', (err, data) => {
 //             if (err) {
 //                 console.error(err);
 //                 res.end('Server Error');
@@ -38,37 +38,77 @@ const fs = require('fs');
  * The following snippet shows how to use connecting functin to avoid callback hell
  *
  */
+// http.createServer((req, res) => {
+//     getTitle(res);
+// }).listen(8000, '127.0.0.1');
+
+// function getTitle(res) {
+//     fs.readFile('./titles.json', (err, data) => {
+//         if (err) {
+//             hadError(err, res);
+//         } else {
+//             getTemplate(JSON.parse(data.toString()), res);
+//         }
+//     });
+// }
+
+// function getTemplate(titles, res) {
+//     fs.readFile('./template.html', (err, data) => {
+//         if (err) {
+//             hadError(err, res);
+//         } else {
+//             formatHtml(titles, data.toString(), res);
+//         }
+//     });
+// }
+
+// function formatHtml(titles, templ, res) {
+//     const html = templ.replace('%', titles.join('</li><li>'));
+//     res.writeHead(200, {'Content-Type': 'text/html'});
+//     res.end(html);
+// }
+
+// function hadError(err, res) {
+//     console.log(err);
+//     res.end('Server Error');
+// }
+
+
+/**
+ *
+ * The following snippet shows we can just RETURN the error and stop the function running
+ * if there's error.
+ *
+ * One thing need to be careful is that, it's return the error handling function, not just run the function.
+ * Because if we just run the funcion without certain termination, I guess the code will crash...
+ * And the best way to stop things running at certain point is return it? I guess😅
+ *
+ */
 http.createServer((req, res) => {
-    getTitle(res);
+    getTitles(res);
 }).listen(8000, '127.0.0.1');
 
-function getTitle(res) {
-    fs.readFile('./title.json', (err, data) => {
-        if (err) {
-            hadError(err, res);
-        } else {
-            getTemplate(JSON.parse(data.toString()), res);
-        }
-    });
+function getTitles(res) {
+   fs.readFile('./titles.json', (err, data) => {
+       if (err) return hadErrors(err, res);
+       getTemplate(JSON.parse(data.toString()), res);
+   });
 }
 
 function getTemplate(titles, res) {
     fs.readFile('./template.html', (err, data) => {
-        if (err) {
-            hadError(err, res);
-        } else {
-            formatHtml(titles, data.toString(), res);
-        }
+        if (err) return hadErrors(err, res);
+        formatHtml(titles, data.toString(), res);
     });
 }
 
 function formatHtml(titles, templ, res) {
     const html = templ.replace('%', titles.join('</li><li>'));
-    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.writeHead(200, { 'Content-Type': 'text/html' });
     res.end(html);
 }
 
-function hadError(err, res) {
-    console.log(err);
+function hadErrors(err, res) {
+    console.error(err);
     res.end('Server Error');
 }
