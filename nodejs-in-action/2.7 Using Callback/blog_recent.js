41aa852af094474430ef70843966130de07208a2
Benjamin <jijileeeeeee@gmail.com>
2019-11-05 13:12:53 +0800

    Just some code snippet I collect with my personal notes during reading books Node.js in Action or maybe something else later, who knows🤪

---
+++ nodejs-in-action/2.7 Using Callback/blog_recent.js
@@ -0,0 +1,27 @@
+const http = require('http');
+const fs = require('fs');
+
+http.createServer((req, res) => {
+    if (req.url === '/') {
+        fs.readFile('./title.json', (err, data) => {
+            if (err) {
+                console.error(err);
+                res.end('Server Error');
+            } else {
+                const titles = JSON.parse(data.toString());
+                console.log('titles = ', titles);
+                fs.readFile('./template.html', (err, data) =>{
+                    if (err) {
+                        console.error(err);
+                        res.end('Server Error');
+                    } else {
+                        const templ = data.toString();
+                        const html = templ.replace('&', titles.join('</li><li>'));
+                        res.writeHead(200, { 'Content-Type': 'text/html' });
+                        res.end(html);
+                    }
+                });
+            }
+        });
+    }
+}).listen(8000, '127.0.0.1');
