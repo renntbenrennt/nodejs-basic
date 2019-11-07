c0a14a4e7aa07521a286c5610257f0068732341b
Benjamin <jijileeeeeee@gmail.com>
2019-11-07 23:59:08 +0800

    From book to code... DONE

---
+++ nodejs-in-action/2.12 /index.js
@@ -0,0 +1,67 @@
+const fs = require('fs');
+const request = require('request');
+const htmlparser = require('htmlparser');
+
+const configFilename = './rss_feeds.txt';
+
+function checkForRSSFile() {
+    fs.exists(configFilename, (exists) => {
+        if (!exists)
+            return next(new Error(`Missing RSS file: ${configFilename}`));
+        next(null, configFilename);
+    });
+}
+
+function readRSSFile(configFilename) {
+    fs.readFile(configFilename, (err, feedlist) => {
+        if (err) return next(err);
+
+        feedlist = feedlist
+            .toString()
+            .replace(/^\s+|\s+$/g, '')
+            .split('\n');
+
+        const random = Math.floor(Math.random() * feedlist.length);
+
+        next(null, feedlist[random]);
+    });
+}
+
+function downloadRSSFeed(feedUrl) {
+    request({ uri: feedUrl }, (err, res, body) => {
+        if (err) return next(err);
+        if (res.statusCode !== 200)
+            return next(new Error('Abnormal response status code'));
+        next(null, body);
+    });
+}
+
+function parseRSSFeed(rss) {
+    const handler = new htmlparser.RssHandler();
+    const parser = new htmlparser.Parser(handler);
+    parser.parseComplete(rss);
+
+    if (!handler.dom.items.length)
+        return next(new Error('No RSS items found'));
+
+    const item = handler.dom.items.shift();
+    console.log(item.title);
+    console.log(item.link);
+}
+
+const tasks = [
+    checkForRSSFile,
+    readRSSFile,
+    downloadRSSFeed,
+    parseRSSFeed
+];
+
+function next(err, result) {
+    if (err) throw err;
+    const currentTask = task.shift();
+    if (currentTask) {
+        currentTask(result);
+    }
+}
+
+next();
