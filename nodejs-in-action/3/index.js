cfe89b878154ccda96b7444d6e3fa6df4df60c80
Benjamin <jijileeeeeee@gmail.com>
2019-11-09 17:49:44 +0800

    Add html template engine EJS

--- nodejs-in-action/3/index.js
+++ nodejs-in-action/3/index.js
@@ -13,7 +13,14 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get('/articles', (req, res, next) => {
     Article.all((err, articles) => {
         if (err) return next(err);
-        res.send(articles);
+        res.format({
+            html: () => {
+                res.render('articles.ejs', { articles: articles });
+            },
+            json: () => {
+                res.send(articles);
+            }
+        });
     });
 });
 
