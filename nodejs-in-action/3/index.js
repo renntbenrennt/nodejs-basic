d84fab6160316277ed05783ee50cb854eb926ad6
Benjamin <jijileeeeeee@gmail.com>
2019-11-09 13:17:34 +0800

    Implement basic web server function: GET

--- nodejs-in-action/3/index.js
+++ nodejs-in-action/3/index.js
@@ -1,12 +1,32 @@
 const express = require('express');
 const app = express();
+const articles = [{ title: 'Example' }];
 
-const port = process.env.PORT || 3000;
+app.set('port', process.env.PORT || 3000);
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
+app.get('/articles', (req, res, next) => {
+    res.send(articles);
 });
 
-app.listen(port, () => {
-    console.log(`Express web app available at localhost: ${port}`);
+app.post('/articles', (req, res, nex) => {
+    res.send('OK');
 });
+
+app.get('/articles/:id', (req, res, next) => {
+    const id = req.params.id;
+    console.log('Fetching: ', id);
+    res.send(articles[id]);
+});
+
+app.delete('/articles/:id', (req, res, next) => {
+    const id = req.params.id;
+    console.log('Deleting: ', id);
+    delete articles[id];
+    res.send({ message: 'Delete' });
+});
+
+app.listen(app.get('port'), () => {
+    console.log(`App started on port`, app.get('port'));
+});
+
+module.exports = app;
