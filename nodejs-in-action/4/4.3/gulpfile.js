f38ea966f97a127f80a6d4533b8b8d1dfbf405c6
Benjamin <jijileeeeeee@gmail.com>
2019-11-13 19:34:41 +0800

    Basic gulp setup for project
    Including:
    1. converting to old js Environment using babel, which
    2. should be noted that, in the Nodejs in action book
    3. it was taught or showed with babel core 6, which
    4. will produce error saying '@babel/core' not found stuff
    5. solution will be install '@babel/core' and '@babel/preset-env'
    6. and something else like '@babel/preset-react'
    
    after these... I found Front end tech stuff is extremely unstable😭

---
+++ nodejs-in-action/4/4.3/gulpfile.js
@@ -0,0 +1,23 @@
+const gulp = require('gulp');
+const sourcemaps = require('gulp-sourcemaps');
+const babel = require('gulp-babel');
+const concat = require('gulp-concat');
+
+gulp.task('default', () => {
+    return gulp.src('app/*.jsx')
+    .pipe(sourcemaps.init())
+    .pipe(babel({
+        presets: ['@babel/preset-env', "@babel/preset-react"]
+        
+        /// The following preset thing is for babel core 6
+        /// and what was said the book Nodejs in action 2
+        /// are for core 6!!!!
+        /// DAMN!!!!
+        // presets: ["@babel/preset-env"]
+
+    }))
+    .pipe(concat('all.js'))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('dist'));
+});
+
